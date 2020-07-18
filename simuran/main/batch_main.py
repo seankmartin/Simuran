708cb8817e4306bcd34ae44b6bf291053b47db18
Sean Martin <seankieran.m@hotmail.com>
2020-07-18 20:48:57 +0100

    Improved plot

--- simuran/main/batch_main.py
+++ simuran/main/batch_main.py
@@ -3,6 +3,7 @@ import os
 
 from simuran.main.main import run
 from simuran.param_handler import ParamHandler
+from neurochat.nc_utils import smooth_1d
 
 # TODO this just needs a way to define the relationship between the recordings
 # For example, you may want to concatenate them, or take the average of them,
@@ -118,14 +119,34 @@ def plot_all_lfp(info, out_dir):
 
     # TODO this should be a helper probably
     parsed_info = []
+    control_data = []
+    lesion_data = []
     for item in info:
         for val in item:
             this_item = list(val.values())[0]
+            to_use = this_item
             this_item[1][-10:] = this_item[1][-20:-10]
-            parsed_info.append(this_item)
+            to_use[1] = smooth_1d(
+                this_item[1].astype(float), kernel_type="hg", kernel_size=5)
+            if this_item[2][0] == "Control":
+                control_data.append(to_use[1])
+            else:
+                lesion_data.append(to_use[1])
+            x_data = to_use[0].astype(float)
+            parsed_info.append(to_use)
+
+    lesion_arr = np.array(lesion_data).astype(float)
+    control_arr = np.array(control_data).astype(float)
+
+    y_lesion = np.average(lesion_arr, axis=0)
+    y_control = np.average(control_arr, axis=0)
+
+    difference = y_control - y_lesion
 
     data = np.concatenate(parsed_info[:-1], axis=1)
     df = pd.DataFrame(data.transpose(), columns=["frequency", "coherence", "Group"])
+    df.replace("Control", "Control (ATN,   N = 6)", inplace=True)
+    df.replace("Lesion",  "Lesion  (ATNx, N = 5)", inplace=True)
     df[["frequency", "coherence"]] = df[["frequency", "coherence"]].apply(pd.to_numeric)
 
     sns.lineplot(
@@ -145,3 +166,18 @@ def plot_all_lfp(info, out_dir):
     plt.ylim(0, 1)
 
     plt.savefig(os.path.join(out_dir, "coherence_full.png"), dpi=400)
+
+    plt.close("all")
+
+    sns.set_style("ticks")
+    sns.set_palette("colorblind")
+
+    sns.lineplot(x=x_data, y=difference)
+
+    sns.despine()
+
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Difference")
+
+    print("Saving to {}".format((os.path.join(out_dir, "difference.pdf"))))
+    plt.savefig(os.path.join(out_dir, "difference.pdf"), dpi=400)
