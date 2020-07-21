3654757353e3e807d3092bee23b57bd606cecb05
Sean Martin <martins7@tcd.ie>
2020-07-21 16:52:26 +0100

    Update recording docs

--- simuran/recording.py
+++ simuran/recording.py
@@ -12,7 +12,59 @@ from skm_pyutils.py_config import split_dict
 
 
 class Recording(BaseSimuran):
+    """
+    Describe a full recording session and holds data.
+
+    This holds single unit information, spatial information,
+    EEG information, stimulation information, and
+    event information.
+
+    Note that anything you want to store on this object that
+    has not been accounted for in attributes, then this can
+    be stored on self.info
+
+    Attributes
+    ----------
+    signals : list of simuran.base_signal.BaseSignal
+        The signals in the recording.
+    units : list of simuran.single_unit.SingleUnit
+        The single units in the recording.
+    spatial : list of simuran.spatial.Spatial
+        The spatial information in the recording.
+    stimulation : list of TODO
+        The stimulation, events, and stimuli in the recording.
+    available : list of strings
+        Each value in the list should be present if the information is available.
+        E.g. available = ["signals", "spatial"] would indicate
+        that this recording has EEG information and spatial information only.
+    param_handler : simuran.param_handler.ParamHandler
+        Parameters which describe the information in the recording.
+    source_file : str
+        The path to the underlying source file describing the recording.
+        When recordings have many source files, this should be either
+        the directory where they are all located, or a file listing them.
+    source_files : dict
+        A dictionary describing the source files for each attribute.
+
+    Parameters
+    ----------
+    params : dict, optional
+        Direct parameters which describe the recording, default is None
+    param_file : str, optional
+        The path to a file which contains parameters, default is None
+    base_file : str, optional
+        Sets the value of self.source_file, default is None
+    load : bool, optional
+        Whether to load the recording on initialisation, default is True
+
+    See also
+    --------
+    simuran.base_class.BaseSimuran
+
+    """
+
     def __init__(self, params=None, param_file=None, base_file=None, load=True):
+        """See help(Recording)."""
         super().__init__()
         self.signals = None
         self.units = None
@@ -28,16 +80,32 @@ class Recording(BaseSimuran):
             self._setup_from_dict(params, load=load)
 
     def load(self, *args, **kwargs):
+        """Load each available attribute."""
         for item in self.get_available():
             item.load()
 
     def get_available(self):
+        """Get the available attributes."""
         return [getattr(self, item) for item in self.available]
 
     def set_base_file(self, base):
+        """Set the source file of this recording."""
         self.source_file = base
 
     def get_signal_channels(self, as_idx=False):
+        """
+        Get the channel of each signal in the recording.
+
+        Parameters
+        ----------
+        as_idx : bool, optional
+            If true, just returns [i for i in range(num_signals)]
+
+        Returns
+        -------
+        list
+
+        """
         num_sigs = self.param_handler["signals"]["num_signals"]
         if as_idx:
             return [i for i in range(num_sigs)]
@@ -46,6 +114,27 @@ class Recording(BaseSimuran):
         return chans
 
     def get_name_for_save(self, rel_dir=None):
+        """
+        Get the name of the recording for saving purposes.
+
+        This is very useful when plotting.
+        For example, if the recording is in foo/bar/foo/pie.py
+        Then passing rel_dir as foo, would return the name as
+        bar--foo--pie
+
+        Parameters
+        ----------
+        rel_dir, str, optional
+            The directory to take the path relative to, default is None.
+            If None, it returns the full path with os.sep replaced by --
+            and with no extension.
+
+        Returns
+        -------
+        str
+            The name for saving, it has no extension and os.sep replaced by --
+
+        """
         if rel_dir is None:
             base_name_part, _ = os.path.splitext(os.path.basename(self.source_file))
         else:
@@ -55,15 +144,23 @@ class Recording(BaseSimuran):
         return base_name_part
 
     def get_available_units(self):
-        l = []
+        """Get the list of available units."""
+        all_units = []
         for i, unit in enumerate(self.units):
-            l.append([unit.group, unit.get_available_units()])
-        return l
+            all_units.append([unit.group, unit.get_available_units()])
+        return all_units
 
     def get_set_units(self):
+        """Get the units which are set for analysis."""
         return [unit.units_to_use for unit in self.units]
 
     def _parse_source_files(self):
+        """
+        Set the value of self.source_files based on the parameters.
+
+        This only functions for things that have been set as available.
+
+        """
         source_files = {}
         for item, name in zip(self.get_available(), self.available):
             if isinstance(item, GenericContainer):
@@ -73,14 +170,57 @@ class Recording(BaseSimuran):
         self.source_files = source_files
 
     def _setup_from_file(self, param_file, load=True):
+        """
+        Set up this recording from a source parameter file.
+
+        Parameters
+        ----------
+        param_file : str
+            The parameter file to setup from.
+        load : bool, optional
+            Whether the information should be loaded, by default True
+
+        Returns
+        -------
+        None
+
+        """
         self.param_handler = ParamHandler(in_loc=param_file)
         self._setup(load=load)
 
     def _setup_from_dict(self, params, load=True):
+        """
+        Set up this recording from a dictionary of parameters.
+
+        Parameters
+        ----------
+        params : dict
+            The parameters to setup from.
+        load : bool, optional
+            Whether the information should be loaded, by default True
+
+        Returns
+        -------
+        None
+
+        """
         self.param_handler = ParamHandler(params=params)
         self._setup(load=load)
 
     def _setup(self, load=True):
+        """
+        Set up this recording.
+
+        Parameters
+        ----------
+        load : bool, optional
+            Whether the information should be loaded, by default True
+
+        Returns
+        -------
+        None
+
+        """
         if self.source_file is None:
             default_base_val = None
             if self.param_handler.location is not None:
@@ -154,6 +294,7 @@ class Recording(BaseSimuran):
         self.valid = True
 
     def __str__(self):
+        """Call on print."""
         return "{} with params {} and source files {}".format(
             self.__class__.__name__, self.param_handler.params, self.source_files
         )
