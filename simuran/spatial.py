e066788516e73a66c37fd552874f989c8a3041d0
Sean Martin <martins7@tcd.ie>
2020-07-21 20:53:18 +0100

    Update spatial docs

--- simuran/spatial.py
+++ simuran/spatial.py
@@ -1,11 +1,24 @@
+"""This module hands holding spatial information."""
+
 from simuran.base_class import BaseSimuran
 
 
 class Spatial(BaseSimuran):
+    """
+    Hold spatial information.
+
+    TODO
+    ----
+    This is a barebones implementation of the spatial information.
+
+    """
+
     def __init__(self):
+        """See help(Spatial)."""
         super().__init__()
 
     def load(self, *args, **kwargs):
+        """Load the spatial information."""
         super().load()
         if not self.loaded():
             load_result = self.loader.load_spatial(self.source_file, **kwargs)
