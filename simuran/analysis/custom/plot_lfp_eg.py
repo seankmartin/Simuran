e24c7ecbf887da2e4443908fd630bad404e8ab40
Sean Martin <martins7@tcd.ie>
2020-08-05 17:17:47 +0100

    Remove a dependency

--- simuran/analysis/custom/plot_lfp_eg.py
+++ simuran/analysis/custom/plot_lfp_eg.py
@@ -1,10 +1,11 @@
 import os
+import math
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-from bvmpc.lfp_odict import LfpODict
-from bvmpc.lfp_plot import plot_lfp
 from simuran.plot.figure import SimuranFigure
+from neurochat.nc_utils import butter_filter
 
 
 def get_normalised_diff(s1, s2):
@@ -13,25 +14,60 @@ def get_normalised_diff(s1, s2):
 
 def main(recording, figures, base_dir):
     location = os.path.splitext(recording.source_file)[0]
-    lfp_odict = LfpODict(
-        location,
-        filt_params=(True, 1.5, 90),
-        channels=[i + 1 for i in range(len(recording.signals))],
-    )
-    lfp_figs = plot_lfp("", lfp_odict, filt=True, return_figs=True)
-    for item in lfp_figs:
-        figure, name = item
-        name = (
-            "--".join(os.path.dirname(location)[len(base_dir + os.sep) :].split(os.sep))
-            + "--"
-            + name
-        )
-        figures.append(SimuranFigure(figure, name, dpi=40, format="png", done=True))
 
     sub_signals = recording.signals.group_by_property("region", "SUB")[0]
     sub_signals = [s for s in sub_signals if not np.all((s.samples == 0))]
     rsc_signals = recording.signals.group_by_property("region", "RSC")[0]
     rsc_signals = [s for s in rsc_signals if not np.all((s.samples == 0))]
+
+    recording_signals = [
+        butter_filter(
+            lfp.get_samples(),
+            recording.signals[0].get_sampling_rate(),
+            10,
+            1.5,
+            90,
+            "bandpass",
+        )
+        for lfp in recording.signals
+    ]
+    in_range = (0, max([lfp.underlying.get_duration() for lfp in recording.signals]))
+    y_axis_max = max([max(lfp) for lfp in recording_signals])
+    y_axis_min = min([min(lfp) for lfp in recording_signals])
+    seg_splits = np.arange(in_range[0], in_range[1], 100)
+    if np.abs(in_range[1] - seg_splits[-1]) > 0.0001:
+        seg_splits = np.concatenate([seg_splits, [in_range[1]]])
+        max_split_len = max(np.diff(seg_splits))
+
+    for j, split in enumerate(seg_splits[:-1]):
+        fig, axes = plt.subplots(
+            nrows=len(recording.signals), figsize=(40, len(recording.signals) * 2)
+        )
+        a = np.round(split, 2)
+        b = np.round(min(seg_splits[j + 1], in_range[1]), 2)
+        out_name = "--".join(
+            os.path.dirname(location)[len(base_dir + os.sep) :].split(os.sep)
+        ) + "raw--0_{}_{:.2f}s_to_{:.2f}s.png".format(j, a, b)
+        for i, lfp in enumerate(recording_signals):
+            convert = recording.signals[0].get_sampling_rate()
+            c_start, c_end = math.floor(a * convert), math.floor(b * convert)
+            lfp_sample = lfp[c_start:c_end]
+            x_pos = recording.signals[i].get_timestamps()[c_start:c_end]
+            axes[i].plot(x_pos, lfp_sample, color="k")
+            axes[i].text(
+                0.03,
+                1.02,
+                "Channel " + str(i + 1),
+                transform=axes[i].transAxes,
+                color="k",
+                fontsize=15,
+            )
+            axes[i].set_ylim(y_axis_min, y_axis_max)
+            axes[i].tick_params(labelsize=12)
+            axes[i].set_xlim(a, a + max_split_len)
+            figures.append(
+                SimuranFigure(fig, out_name, dpi=100, format="png", done=True)
+            )
 
     results = {
         "sub": get_normalised_diff(sub_signals[0].samples, sub_signals[1].samples),
