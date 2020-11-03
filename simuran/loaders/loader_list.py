91f824d159c25c789602b3803639106b436d110c
Sean Martin <seankieran.m@hotmail.com>
2020-11-03 14:10:51 +0000

    Better logging of NC import error

--- simuran/loaders/loader_list.py
+++ simuran/loaders/loader_list.py
@@ -5,6 +5,8 @@ Current loaders are:
 1. params_only : only loads parameters from files.
 2. nc_loader : requires the neurochat package to be installed.
 """
+import sys
+import traceback
 
 loaders_dict = {"params_only": "params_only_no_cls"}
 try:
@@ -12,4 +14,5 @@ try:
 
     loaders_dict["nc_loader"] = NCLoader
 except BaseException:
-    print("NeuroChaT is not installed")
+    print("Error importing NeuroChaT:")
+    traceback.print_exc(file=sys.stdout)
