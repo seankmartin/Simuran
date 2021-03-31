8d78b35fb9ca1c866c40fe4aa83f0fa3b866e1dc
Sean Martin <seankieran.m@hotmail.com>
2021-03-31 10:22:15 +0100

    Update base init file

--- simuran/__init__.py
+++ simuran/__init__.py
@@ -1,1 +1,11 @@
 """Package for multi-region analysis."""
+from . import analysis
+from . import loaders
+from . import main
+from . import params
+from . import plot
+
+from .batch_setup import BatchSetup
+from .param_handler import ParamHandler
+from .recording import Recording
+from .recording_container import RecordingContainer
