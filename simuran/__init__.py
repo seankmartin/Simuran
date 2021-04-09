9ce679b69503d21a96b914710cea0e9eae103da6
Sean Martin <seankieran.m@hotmail.com>
2021-04-09 15:06:14 +0100

    Update init imports

--- simuran/__init__.py
+++ simuran/__init__.py
@@ -1,9 +1,15 @@
 """Package for multi-region analysis."""
 from . import analysis
 from . import loaders
-from . import main
 from . import params
 from . import plot
+
+# For main import differently
+from .main.batch_main import batch_run, batch_main
+from .main.copy_params import copy_param_files
+from .main.doit import create_task
+from .main.single_main import run, analyse_files
+from .main.table import index_ephys_files, analyse_cell_list, populate_table_directories
 
 from .batch_setup import BatchSetup
 from .param_handler import ParamHandler
--- simuran/main/__init__.py
+++ simuran/main/__init__.py
@@ -1,6 +1,6 @@
 """This module provides the main entry points."""
-from .batch_main import batch_run
+from .batch_main import batch_run, batch_main
 from .copy_params import copy_param_files
 from .doit import create_task
 from .single_main import run, analyse_files
-from .table import index_ephys_files
+from .table import index_ephys_files, analyse_cell_list, populate_table_directories
