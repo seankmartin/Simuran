ffcfe5294fa6c1b2df24fd0a9fab55d3f1cd19b1
Sean Martin <seankieran.m@hotmail.com>
2022-04-08 11:48:41 +0100

    Added new main file for restructure

---
+++ simuran/main/new_main.py
@@ -0,0 +1,19 @@
+"""This may be a temp, lets see"""
+
+# Pseudo of idea
+
+# Step 1a (optional) - Help to set up table - maybe see table.py
+
+# Step 2 - Read a filtered table, can explore with d-tale etc. before continuing (JASP)
+
+# Step 3 - Iterate over the table performing a fixed function/s with some optional 
+# parameters that change
+
+# Step 4 - Save output of analysis in multiple formats
+# CSV, straight to JASP etc.
+
+# Step 5 - Support to view figures and tables from within the program
+
+# Should support params in multiple formats of metadata
+
+# IMPORTANT NEED TO BRANCH HERE.
