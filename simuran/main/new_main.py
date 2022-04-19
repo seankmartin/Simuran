d6517ed1775c5c3d026e18c2c5191b1e457e3aa9
Sean Martin <seankieran.m@hotmail.com>
2022-04-19 14:51:50 +0100

    Further pseudo of new idea

--- simuran/main/new_main.py
+++ simuran/main/new_main.py
@@ -1,6 +1,13 @@
 """This may be a temp, lets see"""
+from typing import Union, Literal
+from pathlib import Path
+
 import pandas as pd
 from skm_pyutils.py_table import df_from_file
+from allensdk.brain_observatory.behavior.behavior_project_cache import (
+    VisualBehaviorOphysProjectCache,
+)
+import dtale
 
 # TODO should this be one level down
 from simuran.recording_container import RecordingContainer
@@ -8,13 +15,14 @@ from simuran.analysis.analysis_handler import AnalysisHandler
 
 # Pseudo of idea
 
-# TODO params from elsewhere?
-index_location = os.path.join(temp_storage_location, "index.csv")
+input_file_dir = Path(
+    r"D:\AllenBrainObservatory\ophys_data\visual-behavior-ophys-1.0.1"
+)
 
-nc_loader_kwargs = {
-    "system": "Axona",
-    "pos_extension": ".pos"
-}
+# TODO params from elsewhere?
+index_location = input_file_dir / "index.csv"
+
+nc_loader_kwargs = {"system": "Axona", "pos_extension": ".pos"}
 
 clean_kwargs = {
     "pick_property": "group",
@@ -26,7 +34,6 @@ all_params = {
     # Cleaning params
     "clean_method": "pick_zscore",
     "clean_kwargs": clean_kwargs,
-    
     # Filtering params
     "fmin": 1,
     "fmax": 100,
@@ -35,70 +42,94 @@ all_params = {
     "delta_min": 1.5,
     "delta_max": 4,
     "fmax_plot": 40,
-
     # Plotting params
     "psd_scale": "decibels",
     "image_format": "png",
-
     # Path setup
-    "cfg_base_dir" : "/content/drive/My Drive/NeuroScience/ATN_CA1",
-
+    "cfg_base_dir": "/content/drive/My Drive/NeuroScience/ATN_CA1",
     # STA
-    "number_of_shuffles_sta": 5
+    "number_of_shuffles_sta": 5,
 }
 
 # Step 1a (optional) - Help to set up table - maybe see table.py
-table = df_from_file(index_location)
+def setup_table(input_file_dir: Union[str, Path]) -> pd.DataFrame:
+    cache = VisualBehaviorOphysProjectCache.from_s3_cache(cache_dir=input_file_dir)
+    experiments = cache.get_ophys_experiment_table()
+    dtale.show(experiments, subprocess=False)
+    return experiments
 
-## Series of help on selecting from the dataframe - see
-# https://pandas.pydata.org/docs/user_guide/indexing.html
-# https://pandas.pydata.org/docs/user_guide/basics.html#dt-accessor
-# Easiest way to filter is probably like this
 
-values = {"col1": ["options"], "col2": ["options"]}
-table.isin(values) # or not with table.~isin
-row_mask = table.isin(values).any() # or not with table.~isin
-filtered = table[row_mask]
+# Step 1b filter the table
+def filter_table(table: pd.DataFrame, inplace: bool = True) -> pd.DataFrame:
+    """
+    _summary_
 
-# Alternatively, you can query
+    Parameters
+    ----------
+    table : pd.DataFrame
+        _description_
+    inplace : bool, optional
+        _description_, by default True
 
-var = "b"
-table[(table["a"] == 1) & table[(table[var] == 2)]]
-table.query("a == 1 & @var == 2")
+    Returns
+    -------
+    pd.DataFrame
+        _description_
+    """
+    # Alternatively
+    # values = {"col1": ["options"], "col2": ["options"]}
+    # table.isin(values) # or not with table.~isin
+    # row_mask = table.isin(values).any() # or not with table.~isin
+    # filtered = table[row_mask]
+    genotype = "Slc17a7-IRES2-Cre/wt;Camk2a-tTA/wt;Ai93(TITL-GCaMP6f)/wt"
+    query_ = (
+        "project_code == VisualBehaviorMultiscope" "&" f"full_genotype == {genotype}"
+    )
+    if inplace:
+        table.query(query_, inplace=True)
+        return table
+    else:
+        return table.query(query_, inplace=False)
 
-# Can expand the table with metadata
-expand_table(table)
-filtered_table = table[]
 
-# Step 2 - Read a filtered table, can explore with d-tale etc. before continuing (JASP)
+def establish_analysis():
+    # Temp fn here
+    def print_info(recording, *args, **kwargs):
+        print(recording)
+        print(*args, **kwargs)
+        return recording.source_file
 
-recording_container = RecordingContainer.from_table(
-    table, "neurochat", param_dir, load=False)
+    ah = AnalysisHandler()
+    ah.add_fn(print_info)
 
-# Step 3 - Iterate over the table performing a fixed function/s with some optional 
-# parameters that change
+    return ah
 
-# Temp fn here
-def print_info(recording, *args, **kwargs):
-    print(recording)
-    print(*args, **kwargs)
-    return recording.source_file
 
-ah = AnalysisHandler()
-ah.add_fn(print_info)
+def main():
+    # Should support params in multiple formats of metadata
+    table = setup_table(input_file_dir)
 
-# Step 4 - Save output of analysis in multiple formats
-# CSV, straight to JASP etc.
+    # Step 2 - Read a filtered table, can explore with d-tale etc. before continuing (JASP)
+    filtered_table = filter_table(table)
+    rc = recording_container.from_table(filtered_table, "allen")
+    # TODO I think this step is awkward
 
-output_location = "results.csv"
-ah.save_results(output_location)
+    # Step 3 - Iterate over the table performing a fixed function/s with some optional
+    # parameters that change
+    ah = establish_analysis()
+    ah.run_all_fns()
 
-# Step 5 - Support to view figures and tables from within the program
+    # Step 4 - Save output of analysis in multiple formats
+    # CSV, straight to JASP etc.
+    output_location = "results.csv"
+    ah.save_results(output_location)
 
-# TODO no support for that yet
+    # Step 5 - Support to view figures and tables from within the program
 
-# Should support params in multiple formats of metadata
+    # TODO no support for that yet
 
-# IMPORTANT NEED TO BRANCH HERE.
+    # What about interactions between objects in recordings?
 
-# What about interactions between objects in recordings?
+
+if __name__ == "__main__":
+    main()
