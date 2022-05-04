a5ba697c9264fc0bbfee8864f206822d963a6a88
Sean Martin <seankieran.m@hotmail.com>
2022-05-04 12:33:38 +0100

    New ophys explorer

--- examples/interactive-ophys.py
+++ examples/interactive-ophys.py
@@ -1,4 +1,8 @@
 # %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
 from pathlib import Path
 
 import dtale
@@ -7,9 +11,8 @@ import numpy as np
 import objexplore
 import pandas as pd
 import simuran as smr
-from allensdk.brain_observatory.behavior.behavior_project_cache import (
-    VisualBehaviorOphysProjectCache,
-)
+from allensdk.brain_observatory.behavior.behavior_project_cache import \
+    VisualBehaviorOphysProjectCache
 from skm_pyutils.py_plot import GridFig
 
 # %%
@@ -50,16 +53,19 @@ def filter_table(table: pd.DataFrame) -> pd.DataFrame:
 def plot_mpis(recording_container):
     name = recording_container.metadata["container_id"]
     gf = GridFig(len(recording_container))
-    for recording in recording_container:
-        dataset = recording.signals.data
+    for i in range(len(recording_container)):
+        recording = recording_container.load(i)
+        dataset = recording.data
         ax = gf.get_next()
         ax.imshow(dataset.max_projection.data, cmap="gray")
-        ax.set_title(name)
+        id_ = recording.metadata["ophys_experiment_id"]
+        s = recording.metadata["session_number"]
+        ax_title = f"ID: {id_}, S: {s}"
+        ax.set_title(ax_title)
     out_path = OUTPUT_DIR / "mpis" / f"{name}.png"
     out_path.parent.mkdir(parents=True, exist_ok=True)
     gf.fig.savefig(out_path, dpi=400)
     plt.close(gf.fig)
-
 
 # %%
 data_storage_directory = Path(r"D:\AllenBrainObservatory\ophys_data")
@@ -77,32 +83,20 @@ group_obj = filtered_table.groupby("ophys_container_id")
 for g in group_obj:
     name, df = g
     rc = smr.RecordingContainer.from_table(df, loader)
+    ## RC needs genericcontainer to be dataclass - look tomorrow
     rc.metadata["container_id"] = name
     plot_mpis(rc)
 
-row = filtered_table.iloc[0]
-row_as_dict = row.to_dict()
-row_as_dict[filtered_table.index.name] = row.name
+# %% Explore the RC
+rc.inspect()
+
+# %% explore cache
+print(cache.current_manifest())
+# print(cache.data_path(1085673715))
+# smr.inspect(cache, methods="True")
+# smr.inspect(cache.fetch_api.cache, methods="True")
+print(cache.fetch_api.cache.file_id_column)
+print(filtered_table["file_id"].iloc[0])
+print(cache.fetch_api.cache.data_path(filtered_table["file_id"].iloc[0]))
 
 # %%
-recording = smr.Recording()
-recording.inspect(methods=True)
-all_attrs_and_methods = recording.get_attrs_and_methods()
-print(all_attrs_and_methods)
-
-# %% Experiment checking
-experiment = cache.get_behavior_ophys_experiment(int(row.name))
-print(smr.inspect(experiment))
-
-# %%
-loader = smr.loader("allen_ophys")(cache)
-recording.loader = loader
-recording.metadata = row_as_dict
-recording.available = ["signals"]  # This line is silly
-recording.load()  # Set back to old load
-
-# %%
-recording.inspect()
-# recording.show_table(recording.signals.cell_specimen_table)
-# %% working with the container
-container_ids = rc.table["ophys_container_id"]
