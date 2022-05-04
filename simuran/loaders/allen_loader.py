10633eac445637cb770feca695466ac5b307e1cf
Sean Martin <seankieran.m@hotmail.com>
2022-05-04 12:33:18 +0100

    Experimenting with custom NWB allen loader

--- simuran/loaders/allen_loader.py
+++ simuran/loaders/allen_loader.py
@@ -1,15 +1,42 @@
-import re
+from dataclasses import dataclass
+from os.path import splitext
+from typing import TYPE_CHECKING
 
-from allensdk.brain_observatory.behavior.behavior_project_cache import (
-    VisualBehaviorOphysProjectCache,
-)
+if TYPE_CHECKING:
+    from allensdk.brain_observatory.behavior.behavior_project_cache import (
+        VisualBehaviorOphysProjectCache,
+    )
+
 from simuran.loaders.base_loader import BaseLoader
 from simuran.spatial import Spatial
 
 
+@dataclass
 class AllenOphysLoader(BaseLoader):
-    def __init__(self, cache):
-        self.cache = cache
+    """
+    Load AIS ophys data from a cache.
+
+    Attributes
+    ----------
+    cache : VisualBehaviorOphysProjectCache
+        The AIS ophys cache to load data from.
+
+    """
+
+    cache: "VisualBehaviorOphysProjectCache"
+
+    # TODO consider including this using inbuilt allen functions
+    # def path_to_nwb(self, recording) -> str:
+    #     manifest_file = self.cache.current_manifest()
+    #     manifest_version = splitext(manifest_file)[0].split("_")[-1][1:]
+    #     id_ = recording.metadata["ophys_experiment_id"]
+    #     fname = (
+    #         DATA_STORAGE_DIRECTORY
+    #         / f"visual-behavior-ophys-{manifest_version}"
+    #         / "behavior_ophys_experiments"
+    #         / f"behavior_ophys_experiment_{id_}.nwb"
+    #     )
+    #     return fname
 
     def parse_table_row(self, table, index, recording) -> None:
         """Move information from row into recording."""
@@ -20,7 +47,6 @@ class AllenOphysLoader(BaseLoader):
         recording.metadata = row_as_dict
         recording.available = ["data", "spatial"]
 
-    ## TODO experimenting with a new idea here
     def load_recording(self, recording) -> None:
         ophys_experiment_id = recording.metadata["ophys_experiment_id"]
         experiment = self.cache.get_behavior_ophys_experiment(ophys_experiment_id)
