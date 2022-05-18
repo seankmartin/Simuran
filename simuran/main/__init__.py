651118373261cf75c3c6fabffa5be3e7186f4aa8
Sean Martin <seankieran.m@hotmail.com>
2022-05-18 17:58:18 +0100

    Experimental project restructure

--- simuran/main/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This module provides the main entry points."""
-from .table import (
-    analyse_cell_list,
-    index_ephys_files,
-    main_analyse_cell_list,
-    populate_table_directories,
-)
