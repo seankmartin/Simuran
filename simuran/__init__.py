1000dc6595024cc22a9d83c7fd5e3c619bae9b63
Sean Martin <seankieran.m@hotmail.com>
2022-09-07 23:15:42 +0100

    Remove main function

--- simuran/__init__.py
+++ simuran/__init__.py
@@ -11,7 +11,6 @@ from .eeg import EEG, EEGArray
 from .core.log_handler import log_exception, set_only_log_to_file
 from .core.param_handler import ParamHandler
 from .loaders.loader_list import loader_from_string
-from .main import main_with_data, main_with_files
 from .plot.base_plot import despine, save_simuran_plot, set_plot_style, setup_ax
 from .plot.figure import SimuranFigure
 from .recording import Recording
