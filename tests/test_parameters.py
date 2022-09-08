4620f52ea0e763e759f52e757d910e63b417d7f7
Sean Martin <seankieran.m@hotmail.com>
2022-09-08 16:54:08 +0100

    Update param handler and tests

---
+++ tests/test_parameters.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+from simuran.core.param_handler import ParamHandler
+
+
+@pytest.mark.parametrize("file_type", [(".yaml"), (".json"), (".py")])
+def test_read_write(file_type):
+    params = {"hello_world": "banana", 0: [1, 10, 14.1], "chans": {"1": "b"}}
+    ph_write = ParamHandler(attrs=params)
+    ph_write.write(f"test_simuran_temp{file_type}")
+    ph_read = ParamHandler(source_file=f"test_simuran_temp{file_type}")
+    assert ph_read.attrs["hello_world"] == "banana"
+    if file_type == ".yaml":
+        assert ph_read[0] == [1, 10, 14.1]
+    else:
+        assert ph_read["0"] == [1, 10, 14.1]
+    assert ph_read["chans"]["1"] == "b"
+    os.remove(f"test_simuran_temp{file_type}")
+
+
+def test_dict_passes():
+    ph = ParamHandler()
+    ph.setdefault("a", "val")
+    assert ph.get("a") == "val"
+    assert ph.get("b", "hi") == "hi"
+    assert len(ph.values()) == 1
+    assert len(ph.keys()) == 1
+    ph.pop("a")
+    assert len(ph.items()) == 0
+    ph["1"] = 100
+    assert "1" in ph
+    assert ph.setdefault("1") == 100
+    ph.clear()
+    assert len(ph) == 0
